1db0e924f3003155b5b681dddd257b0d3f33a30c
silvestrevivo <silvestrevivomillan@gmail.com>
2018-12-06 13:41:26 +0100

    setup with ts and webpack

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <div>
+        Hello TS!!
+    </div>,
+    document.getElementById('app')
+)
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.tsx',
-  modules: {
+  module: {
     rules: [
       {
         test: /\.tsx?$/,
